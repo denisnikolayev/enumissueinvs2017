36d57e3af13eb50c8b4ed3ec912473473dd54b5e
Denis Nikolayev <denis.nikolayev@jet.com>
2017-05-19 11:40:23 -0400

    first

---
+++ Library1/Library1.fs
@@ -0,0 +1,6 @@
+﻿namespace Library1
+
+open FSharp.Data
+
+
+type Enum1 = SqlEnumProvider<"select name, id from table1", "Data Source=(LocalDb)\MSSQLLocalDB;Initial Catalog=New Database;Integrated Security=True;">
---
+++ SQLQuery1.sql
@@ -0,0 +1,3 @@
+﻿CREATE TABLE table1 (id INT, name nVARCHAR(MAX));
+INSERT INTO table1 VALUES (1, 'One')
+INSERT INTO table1 values (2, 'Two')
